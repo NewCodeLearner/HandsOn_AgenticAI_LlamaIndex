ae582e641f31c104802d14bf9bc8891e6cebadbe
Girish Barhate <girish.barhate@gmail.com>
2025-03-27 17:51:13 +0530

    create first step for the workflow.

--- 05_simple_worklflow.py
+++ 05_simple_worklflow.py
@@ -36,4 +36,32 @@ class SimpleWorkflow(Workflow):
         super().__init__(*args,**kwargs)
         #store input into instance variables
         self.max_iterations = max_iterations
+
+    # First Step 
+    # When we define StartEvent as the input argument that means its the start/first step for the workflow.
+    @step
+    async def runLoop(self,
+                      ctx:Context,
+                      event:StartEvent | ContinueEvent) -> ValidateEvent:
+        # If StartEvent , initialize the variables
+        if isinstance(event,StartEvent):
+            iterations = 0
+            current_result = ''
+        else:
+            #for ContinueEvent
+            #read current result from Context
+            current_result=await ctx.get("current_result")
+            # Read current iteration count from event
+            iterations =event.iterations
         
+        #increase the iterations
+        iterations = iterations + 1
+        #create current result value
+        current_result = f"*** Iteration : {iterations} {self.max_iterations}"
+        print(current_result)
+
+        #Set current result value in context.
+        await ctx.set("current_result", current_result)
+
+        #Return validate event, with current value of iterations
+        return ValidateEvent(iterations=iterations)
